fa08d409f422d3221122d7e26e8a8a419d6f71a1
Mateusz Bączek <mateusz.baczek1998@gmail.com>
2026-09-16 22:20:55 +0200

    build(flake): add flake.nix

---
+++ flake.nix
@@ -0,0 +1,32 @@
+{
+  description = "dreaming.systems webpage development environment0";
+  inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+
+  outputs = { self, nixpkgs }:
+    let
+      systems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
+      forAllSystems = f: nixpkgs.lib.genAttrs systems (system: f system);
+    in
+    {
+      devShells = forAllSystems (system:
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
+        in
+        {
+          default = pkgs.mkShell {
+            packages = [
+              pkgs.uv
+              pkgs.zola
+            ];
+
+            # LLMs often want to use a Python environment with some popular
+            # libraries for running one-off validation/exploration commands
+            buildInputs = [
+              (pkgs.python3.withPackages (python: [
+                python.pyyaml
+              ]))
+            ];
+          };
+        });
+    };
+}
